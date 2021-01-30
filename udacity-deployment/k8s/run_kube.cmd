5617c4c31f2d87a5cfb98e3227d5aadf198b2440
Zilvinas Marozas <zilvinas_m@hotmail.com>
2021-01-29 19:48:15 -0500

    changes

--- udacity-deployment/k8s/run_kube.cmd
+++ udacity-deployment/k8s/run_kube.cmd
@@ -1,5 +1,6 @@
 kubectl apply -f aws-secret.yaml
 kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
 
 kubectl apply -f backend-feed-deployment.yaml
 kubectl apply -f backend-feed-service.yaml
